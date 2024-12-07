4df5715d42a2817b11fe63f0ff6ccbadbc39f996
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-12-07 16:39:41 +0500

    Task1

--- Tyuiu.CherkashinMM.Sprint6.Task1.V6.Lib/DataService.cs
+++ Tyuiu.CherkashinMM.Sprint6.Task1.V6.Lib/DataService.cs
@@ -2,7 +2,7 @@ using tyuiu.cources.programming.interfaces.Sprint6;
  
 namespace Tyuiu.CherkashinMM.Sprint6.Task1.V6.Lib;
 
-public class DataService
+public class DataService : ISprint6Task1V6
 {
     public double[] GetMassFunction(int startValue, int stopValue)
     {
